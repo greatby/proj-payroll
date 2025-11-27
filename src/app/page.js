82f8ba2943ebe1a6669c54e3d25ef8dc9bc69702
greatby <gills_halogen2i@icloud.com>
2025-11-27 14:19:18 +0530

    minor code changes

--- src/app/page.js
+++ src/app/page.js
@@ -65,7 +65,7 @@ const LandingPage = () => {
         <NotionHero data={dataHero} />
         <AiUseCases />
         <NotionIntro />
-        <BentoCards />
+        {/* <BentoCards /> */}
         <NotionFooter />
       </div>
     </>
